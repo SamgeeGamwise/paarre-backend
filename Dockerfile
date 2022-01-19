a5349e1c9a533a3a4795b1f436fc2444b863e0e0
Samuel Krohn <SANIKR@outlook.com>
2022-01-18 21:52:18 -0700

    Removed Docker

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:16-alpine
-RUN apk add --no-cache git
-WORKDIR /app/
-COPY package.json .
-RUN npm install
-COPY . .
--- src/database/models/_connect.ts
+++ src/database/models/_connect.ts
@@ -1,23 +1,14 @@
 import { Sequelize } from "sequelize"
 import { IIndexable } from "../../types"
 import config from "../config/config"
+
 let sequelize: Sequelize
+const env: string = process.env.NODE_ENV || "development"
 
-if (process.env.CLEARDB_DATABASE_URL) {
-    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
-        dialect: "mysql",
-        protocol: "mysql",
-        dialectOptions: {
-            ssl: true,
-        },
-    })
-} else {
-    const env: string = process.env.NODE_ENV || "development"
+let envConfig: IIndexable = config
+envConfig = envConfig[env]
+console.log(envConfig)
 
-    let envConfig: IIndexable = config
-    envConfig = envConfig[env]
-
-    sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, envConfig)
-}
+sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, envConfig)
 
 export default sequelize
